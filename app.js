89977fef7c5aa80c1ccdd4f8202228ae816180b0
hxnshaw <eghosasere07@gmail.com>
2023-07-05 13:53:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+//Express
+const express = require("express");
+const app = express();
+
+//Database
+const connectDB = require("./db/connect");
+
+const port = process.env.PORT || 2023;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is live on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
